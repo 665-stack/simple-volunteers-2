4fcb40319ecbd24c6d7ae6dddec036c85b2b3d05
MahmodulShahin <mahmodulshahin665@gmail.com>
2022-09-28 12:14:44 +0600

    set same search functionality but using useEffect with dependency

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,22 +1,42 @@
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import useVolunteers from '../../Hooks/useVolunteers';
 import Activity from '../Activity/Activity';
 import './Main.css'
 
 const Main = () => {
     // ami caile setVolantrees take naw import korte pari. Shetar khub akta kaj akhane nai.
-    const [volanteers] = useVolunteers();
+    // const [volanteers] = useVolunteers();
+
+    //akhane searchText ta holo string. mane text. tai default value '' deya hoice.
+    const [searchText, setSearchText] = useState('');
+
     const [searchResult, setSearchResult] = useState([]);
 
+    useEffect(() => {
+        console.log('inside useEffect');
+        fetch('data.json')
+            .then(res => res.json())
+            .then(data => {
+                const match = data.filter(d => d.title.includes(searchText));
+                setSearchResult(match)
+
+            })
+    }, [searchText])
+
     const handleSearchChange = event => {
-        const searchtext = (event.target.value);
-        const match = volanteers.filter(v => v.title.includes(searchtext));
-        setSearchResult(match);
+        setSearchText(event.target.value);
     }
+
+    // 1st system to implement search
+    // const handleSearchChange2 = event => {
+    //     const searchtext = (event.target.value);
+    //     const match = volanteers.filter(v => v.title.includes(searchtext));
+    //     setSearchResult(match);
+    // }
     return (
         <div>
-            <h2>Valunteers activities: {volanteers.length}</h2>
+            <h2>Valunteers activities: {searchResult.length}</h2>
 
             <div className='input-container'>
                 <input onChange={handleSearchChange} className='input' type="text" placeholder='search' />
