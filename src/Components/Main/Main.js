8054edbfd44d8604925fe9b37b51a20eaad09e83
MahmodulShahin <mahmodulshahin665@gmail.com>
2022-09-28 11:28:52 +0600

    implement search box with onchange

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,4 +1,5 @@
 
+import { useState } from 'react';
 import useVolunteers from '../../Hooks/useVolunteers';
 import Activity from '../Activity/Activity';
 import './Main.css'
@@ -6,13 +7,25 @@ import './Main.css'
 const Main = () => {
     // ami caile setVolantrees take naw import korte pari. Shetar khub akta kaj akhane nai.
     const [volanteers] = useVolunteers();
+    const [searchResult, setSearchResult] = useState([]);
+
+    const handleSearchChange = event => {
+        const searchtext = (event.target.value);
+        const match = volanteers.filter(v => v.title.includes(searchtext));
+        setSearchResult(match);
+    }
     return (
         <div>
             <h2>Valunteers activities: {volanteers.length}</h2>
 
+            <div className='input-container'>
+                <input onChange={handleSearchChange} className='input' type="text" placeholder='search' />
+            </div>
+
+
             <div className='activity-container'>
                 {
-                    volanteers.map(activity => <Activity activity={activity} key={activity?.taskId}></Activity>)
+                    searchResult.map(activity => <Activity activity={activity} key={activity?.taskId}></Activity>)
 
                 }
             </div>
